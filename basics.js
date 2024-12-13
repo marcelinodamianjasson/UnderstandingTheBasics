358d8e1d32f7c82a59a0b20f2bcbacc64276b1d5
Marcelino <marcelinodamianjasson@gmail.com>
2024-12-13 15:09:52 +0200

    Controlling the Node.js Process

--- basics.js
+++ basics.js
@@ -1,7 +1,11 @@
+//Want to quit your running Node.js server?You can always do that by pressing CTRL + C in the terminal/ 
+// command prompt window where you started your server (i.e. where you ran node app.js).
+
 const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req);
+    // process.exit();
 });
 
 server.listen(3000);
