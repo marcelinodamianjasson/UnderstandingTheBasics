563dbc1a58630ecf708ddc6e6e63b39db0af3ee5
Marcelino <marcelinodamianjasson@gmail.com>
2024-12-13 16:29:37 +0200

    server with req details

--- basics.js
+++ basics.js
@@ -4,8 +4,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
     // process.exit();
+    res.setHeader('Content-type', 'text/html');
+    res.write('</html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1><body>');
+    res.write('</html>');
+    res.end(); //stop writing here
 });
 
 server.listen(3000);
